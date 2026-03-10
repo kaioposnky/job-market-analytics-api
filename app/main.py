7d5d523420ade83c16f870c33c96e131e844e534
João Abreu <joaog.abreu.10@gmail.com>
2026-03-09 23:27:04 -0300

    feat: initialize database tables on app startup

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from app.database import engine, Base
+from app.models import job
 from app.api.routes import jobs
 
-app = FastAPI()
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Job Market API")
 
 @app.get("/")
 def home():
-    return {"message": "Job Market API running"}
+    return {"message": "Job Market API running and Database Connected"}
 
 app.include_router(jobs.router)
